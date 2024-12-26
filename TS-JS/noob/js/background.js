a5b1e5f5670db6396ae65f70df9df28df68a4b5e
alysion_webstorm <alysion@e8ight.co.kr>
2024-12-26 15:18:01 +0900

    cleanup path

--- TS-JS/noob/js/background.js
+++
@@ -1,8 +0,0 @@
-const images = ["img_0.jpeg", "img_1.jpeg", "img_2.jpeg", "img_3.jpeg", "img_4.jpeg", "img_5.jpeg", "img_6.jpeg", "img_7.jpeg", "img_8.jpeg"];
-
-const chosenImage = images[Math.floor(Math.random() * images.length)];
-
-const bgImage = document.createElement("img");
-bgImage.src = `resource/${chosenImage}`;
-bgImage.className = "background-image";
-document.body.appendChild(bgImage);
